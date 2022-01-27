af18658c92b049335f910124724a62a068af17d7
Carlos Cobian <rcobianm1600@alumno.ipn.x>
2022-01-27 11:20:53 -0600

    Recuperar valores en el display de la calculadora

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -1,14 +1,8 @@
-//
-//  ViewController.swift
-//  Calculator
-//
-//  Created by Angela Yu on 10/09/2019.
-//  Copyright © 2019 London App Brewery. All rights reserved.
-//
 
 import UIKit
 
 class ViewController: UIViewController {
+    var isFinishedTypingNumber: Bool = true
     
     @IBOutlet weak var displayLabel: UILabel!
     
@@ -17,6 +11,7 @@ class ViewController: UIViewController {
     @IBAction func calcButtonPressed(_ sender: UIButton) {
         
         //What should happen when a non-number button is pressed
+        isFinishedTypingNumber = true
     
     }
 
@@ -24,7 +19,14 @@ class ViewController: UIViewController {
     @IBAction func numButtonPressed(_ sender: UIButton) {
         
         //What should happen when a number is entered into the keypad
-    
+        if let numValue = sender.currentTitle{
+            if isFinishedTypingNumber{
+                displayLabel.text = numValue
+                isFinishedTypingNumber = false
+            }else{
+                displayLabel.text! += numValue
+            }
+        }
     }
 
 }
