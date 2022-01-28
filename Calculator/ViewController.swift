3b7750aa5b0c8d5ae24493f4c06a4ffad2d5550d
Carlos Cobian <rcobianm1600@alumno.ipn.x>
2022-01-28 11:45:33 -0600

    Se añadió funcionalidad a los botones principales de la calculadora. Borrar, +/- y porcentaje

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -2,7 +2,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-    var isFinishedTypingNumber: Bool = true
+    private var isFinishedTypingNumber: Bool = true
     
     @IBOutlet weak var displayLabel: UILabel!
     
@@ -12,7 +12,19 @@ class ViewController: UIViewController {
         
         //What should happen when a non-number button is pressed
         isFinishedTypingNumber = true
-    
+        guard let number = Double(displayLabel.text!)else{
+            fatalError("Cannot convert a String into Double")
+        }
+        
+        if let currentMethod = sender.currentTitle{
+            if currentMethod == "+/-"{
+                displayLabel.text = String(number * -1)
+            }else if currentMethod == "AC"{
+                displayLabel.text = "0"
+            }else if currentMethod == "%"{
+                displayLabel.text = String(number/100)
+            }
+        }
     }
 
     
@@ -21,9 +33,17 @@ class ViewController: UIViewController {
         //What should happen when a number is entered into the keypad
         if let numValue = sender.currentTitle{
             if isFinishedTypingNumber{
+                if numValue == "."{
+                    displayLabel.text = "0."
+                }
                 displayLabel.text = numValue
                 isFinishedTypingNumber = false
             }else{
+                if numValue == "."{
+                    if displayLabel.text!.contains("."){
+                        return
+                    }
+                }
                 displayLabel.text! += numValue
             }
         }
